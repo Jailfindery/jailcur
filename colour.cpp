9f6327b1b54f9950cda69023f525f331c5c4008f
Jailfindery <joshua.g.schell@gmail.com>
2013-12-22 16:10:16 -0600

    Added colour enumerations and classes

---
+++ colour.cpp
@@ -0,0 +1,112 @@
+#include <ostream>
+#include <stdexcept>
+
+#include "colour.h"
+
+using namespace std;
+
+namespace jailcur {
+
+/* Initializes a cp_vector object with all zeros. */
+cp_vector::cp_vector()
+{
+    for(int i = 0; i < 8; ++i)
+        V[i] = 0;
+}
+
+}
+
+/* Print colour to ostream */
+ostream& operator<< (ostream& out, const colour& c)
+{
+    switch(c)
+    {
+      case colour::black:   out << "black";
+                            break;
+      case colour::red:     out << "red";
+                            break;
+      case colour::green:   out << "green";
+                            break;
+      case colour::yellow:  out << "yellow";
+                            break;
+      case colour::blue:    out << "blue";
+                            break;
+      case colour::magenta: out << "magenta";
+                            break;
+      case colour::cyan:    out << "cyan";
+                            break;
+      case colour::white:   out << "white";
+                            break;
+      default:              throw runtime_error("jailcur: colour: operator<<(): Invalid colour as parameter");
+                            break;
+    }
+    return out;
+}
+
+/* Increments colour to the next value in terms of integers. If value is
+ * colour::white (or 7), colour::max (or 8) is returned. This is intended for
+ * iteration.
+ *
+ * Note that colour::max can not be incremented and that colour::min
+ * does not need to be handled.
+ */
+void operator++ (colour& c)
+{
+    switch(c)
+    {
+      case colour::black:   c = colour::red;
+                            break;
+      case colour::red:     c = colour::green;
+                            break;
+      case colour::green:   c = colour::yellow;
+                            break;
+      case colour::yellow:  c = colour::blue;
+                            break;
+      case colour::blue:    c = colour::magenta;
+                            break;
+      case colour::magenta: c = colour::cyan;
+                            break;
+      case colour::cyan:    c = colour::white;
+                            break;
+      case colour::white:   c = colour::max;
+                            break;
+      case colour::max:     throw runtime_error(
+                "jailcur: colour: operator++(): Attempted to increment colour::max");
+      default:              throw runtime_error(
+                "jailcur: colour: operator++(): Invalid colour as parameter");
+                            break;
+    }
+}
+
+/* Like operator++(), but for decrementation. It is provided only as the inverse
+ * of operator++() as it is not particularly useful of iteration.
+ */
+void operator-- (colour& c)
+{
+    switch(c)
+    {
+      case colour::min:     throw runtime_error(
+                "jailcur: colour: operator--(): Attempted to decrement colour::min");
+                            break;
+      case colour::red:     c = colour::black;
+                            break;
+      case colour::green:   c = colour::red;
+                            break;
+      case colour::yellow:  c = colour::green;
+                            break;
+      case colour::blue:    c = colour::yellow;
+                            break;
+      case colour::magenta: c = colour::blue;
+                            break;
+      case colour::cyan:    c = colour::magenta;
+                            break;
+      case colour::white:   c = colour::cyan;
+                            break;
+      case colour::max:     c = colour::white;
+                            break;
+      default:              throw runtime_error(
+                "jailcur: colour: operator--(): Invalid colour as parameter");
+                            break;
+    }
+}
+
---
+++ colour.h
@@ -0,0 +1,65 @@
+#ifndef JAILCUR_COLOUR_H_INCLUDED
+#define JAILCUR_COLOUR_H_INCLUDED
+
+#include <ostream>
+
+using namespace std;
+
+namespace jailcur {
+
+/* Used to specify colours for use in jailcur. Integer values for each colour
+ * matches those defined in curses.
+ */
+enum class colour
+{
+    black = 0,
+    red = 1,
+    green = 2,
+    yellow = 3,
+    blue = 4,
+    magenta = 5,
+    cyan = 6,
+    white = 7,
+
+    min = 0,    /* Equal to colour::black */
+    max = 8     /* Equal to colour::white "plus 1" */
+};
+
+/* Used as a data handle for cp_matrix in order to implement
+ * pseudo-operator[][]. It is hard-coded to be an 8-vector.
+ */
+class cp_vector
+{
+  public:
+    cp_vector();
+    int& operator[] (const colour& x) { return V[static_cast<int>(x)]; }
+    int& operator[] (const int& x) { return V[x]; }
+  private:
+    int V[8];
+};
+
+/* Used to store unique colour pair integers which can be looked-up using
+ * a pair of colour values. Uses cp_vector to store rows. It is hard-coded to
+ * be an 8-matrix.
+ */
+class cp_matrix
+{
+  public:
+    cp_matrix() {}  /* Initialized through cp_vector::cp_vector() */
+    cp_vector& operator[] (const colour& x) { return M[static_cast<int>(x)]; }
+    cp_vector& operator[] (const int& x) { return M[x]; }
+  private:
+    cp_vector M[8];
+};
+
+}
+
+using namespace jailcur;
+
+/* colour operations */
+ostream& operator<< (ostream& out, const colour& c);    /* Print to ostream */
+void operator++ (colour& c);                            /* Increment */
+void operator-- (colour& c);                            /* Decrement */
+
+#endif /* JAILCUR_COLOUR_H_INCLUDED */
+
