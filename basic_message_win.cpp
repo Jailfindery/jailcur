80b7f6d4221bb7799db39fa536da0b2ebb63b305
Jailfindery <joshua.g.schell@gmail.com>
2014-02-18 16:27:36 -0600

    Added licensing information

--- basic_message_win.cpp
+++ basic_message_win.cpp
@@ -1,3 +1,11 @@
+/* jailcur: An object-oriented wrapper for curses
+ * Copyright (C) 2014 Joshua Schell <joshua.g.schell@gmail.com>
+ *
+ * This library is licensed under the GNU General Public License version
+ * 3. You should have received a copy of it along with this program.  If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <list>
 
 #include <curses.h>
--- basic_message_win.h
+++ basic_message_win.h
@@ -1,3 +1,11 @@
+/* jailcur: An object-oriented wrapper for curses
+ * Copyright (C) 2014 Joshua Schell <joshua.g.schell@gmail.com>
+ *
+ * This library is licensed under the GNU General Public License version
+ * 3. You should have received a copy of it along with this program.  If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef JAILCUR_BASIC_MESSAGE_WIN_H_INCLUDED
 #define JAILCUR_BASIC_MESSAGE_WIN_H_INCLUDED
 
